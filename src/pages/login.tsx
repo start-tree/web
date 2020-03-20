cc8a046ffa2ded45c85264b27255f8827c30506b
vikei <vikei.dev@gmail.com>
2020-03-20 23:06:12 +0200

    Add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+const loginMutation = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      token
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+const Login = () => {
+  const [login, data] = useMutation(loginMutation);
+  const [form, setForm] = useState({});
+
+  console.log(data);
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+
+          login({ variables: { input: form } });
+        }}
+      >
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            name="email"
+            onChange={e => {
+              setForm({
+                ...form,
+                [e.currentTarget.name]: e.currentTarget.value
+              });
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            name="password"
+            onChange={e => {
+              setForm({
+                ...form,
+                [e.currentTarget.name]: e.currentTarget.value
+              });
+            }}
+          />
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
