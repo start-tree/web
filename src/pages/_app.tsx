922a3d90180cfa82ff8c34c9e96c2de829c537a3
vikei <vikei.dev@gmail.com>
2020-03-21 15:17:03 +0200

    Add material ui to register page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,52 @@
-import React from "react";
-import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { AppProps } from "next/app";
+import React from "react";
 import { CookiesProvider } from "react-cookie";
 import { client } from "../apollo";
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
 const App = ({ Component, pageProps }: AppProps) => {
+  const classes = useStyles();
+
   return (
     <CookiesProvider>
       <ApolloProvider client={client}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
         <Component {...pageProps} />
       </ApolloProvider>
     </CookiesProvider>
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,73 +1,57 @@
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { RegisterInput, useRegisterMutation } from "../apollo/generated";
+import { TextField, Button, FormControl, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+  submitButton: {
+    marginTop: theme.spacing(2)
+  }
+}));
 
 const Register = () => {
+  const classes = useStyles();
   const router = useRouter();
   const [, setCookie] = useCookies(["token"]);
-  const [register] = useRegisterMutation({
+  const [registerMutations] = useRegisterMutation({
     onCompleted: data => {
       setCookie("token", data.register.token);
       router.push("/");
     }
   });
-  const [form, setForm] = useState<RegisterInput>({
-    name: "",
-    email: "",
-    password: ""
-  });
+  const { register, handleSubmit } = useForm<RegisterInput>();
 
   return (
     <div>
       <form
-        onSubmit={e => {
-          e.preventDefault();
-          register({ variables: { input: form } });
-        }}
+        autoComplete="off"
+        onSubmit={handleSubmit(values => {
+          console.log(values);
+
+          registerMutations({ variables: { input: values } });
+        })}
       >
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            onChange={e => {
-              setForm({
-                ...form,
-                [e.currentTarget.name]: e.currentTarget.value
-              });
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="text"
-            name="email"
-            onChange={e => {
-              setForm({
-                ...form,
-                [e.currentTarget.name]: e.currentTarget.value
-              });
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
+        <FormControl fullWidth>
+          <TextField name="name" label="Name" inputRef={register} />
+        </FormControl>
+        <FormControl fullWidth>
+          <TextField name="email" label="Email" inputRef={register} />
+        </FormControl>
+        <FormControl fullWidth>
+          <TextField
             type="password"
             name="password"
-            onChange={e => {
-              setForm({
-                ...form,
-                [e.currentTarget.name]: e.currentTarget.value
-              });
-            }}
+            label="Password"
+            inputRef={register}
           />
-        </div>
-        <div>
-          <button type="submit">Register</button>
-        </div>
+        </FormControl>
+        <FormControl className={classes.submitButton}>
+          <Button type="submit" variant="contained" color="primary">
+            Register
+          </Button>
+        </FormControl>
       </form>
     </div>
   );
